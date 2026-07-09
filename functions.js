27bed82de0917774440ab9b3943abd95896504d0
monteiroux <pd.data.user@gmail.com>
2026-07-08 23:19:15 -0300

    Sobe botão de filtro

--- functions.js
+++ functions.js
@@ -1408,3 +1408,59 @@ $(function () {
     });
 
 });
+
+$(document).ready(function () {
+  
+    /* ======================================================
+       1. CRIA BOTÃO FILTRAR
+    ====================================================== */
+
+    $('.ordenar-listagem.topo .row-fluid').append(`
+        <button class="btn btn-filtrar" data-toggle="modal" data-target="#modalFiltros">
+            Filtrar
+        </button>
+    `);
+
+
+    /* ======================================================
+       2. CRIA MODAL
+    ====================================================== */
+
+    $('body').append(`
+        <div id="modalFiltros" class="modal fade" tabindex="-1" style="display: none;">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+
+                    <div class="modal-header">
+                        <h4 class="modal-title">Filtros</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+
+                    <div class="modal-body">
+                        <div class="modal-ordenar">
+                          <h4>Ordenar por:</h4>
+                        </div>
+                        <div class="modal-filtros"></div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    `);
+
+
+    /* ======================================================
+       3. MOVE DROPDOWN ORDENAR PARA O MODAL
+    ====================================================== */
+
+    $('.ordenar-listagem.topo .dropdown-menu')
+        .appendTo('#modalFiltros .modal-ordenar');
+
+
+    /* ======================================================
+       4. MOVE TODOS OS FILTROS PARA O MODAL
+    ====================================================== */
+
+    $('.filtro-coluna').appendTo('#modalFiltros .modal-filtros');
+
+});
